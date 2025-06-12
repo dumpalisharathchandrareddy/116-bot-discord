d4516485d39c8c870cd5c31463b6b01fc280baf9
Sharath Chandra Reddy Dumpali <CSHARATH301@gmail.com>
2025-06-11 21:12:34 -0400

    1

--- temp.js
+++
@@ -1,8 +0,0 @@
-// db.js now uses pg-pool to manage connections to a PostgreSQL database.
-const { Pool } = require("pg");
-
-const pool = new Pool({
-  connectionString: process.env.PG_URI,
-});
-
-module.exports = pool;
